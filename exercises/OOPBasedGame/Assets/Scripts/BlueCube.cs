707478573cb97ab7d5a57ab5b4fa0187b90d0e05
AsuraIII <57685130+AsuraIII@users.noreply.github.com>
2020-09-14 11:52:16 +0800

    first commit

---
+++ exercises/OOPBasedGame/Assets/Scripts/BlueCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueCube : Cube
+{
+    private string curCubeName = "BlueCube";
+    private int bluePoint = 3;
+
+    private void Start()
+    {
+        cubeName = curCubeName;
+    }
+
+    public override void EarnPoint()
+    {
+        GameManager._instance.AddPoint(bluePoint);
+    }
+
+}
---
+++ exercises/OOPBasedGame/Assets/Scripts/RedCube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class RedCube : Cube
+{
+    public string curCubeName = "RedCube";
+    private int redPoint = 1;
+
+    private void Start()
+    {
+        cubeName = curCubeName;
+    }
+
+
+    public override void EarnPoint()
+    {
+        GameManager._instance.AddPoint(redPoint);
+    }
+
+}
